4272ae5c926cf5e19a8128ae879752ecb44b71a0
prax19 <fhcarpaty2@gmail.com>
2023-12-25 23:40:09 +0100

    Fix 'Could not autowire' error

--- src/main/java/com/prax19/budgetguardapi/BudgetGuardApiApplication.java
+++ src/main/java/com/prax19/budgetguardapi/BudgetGuardApiApplication.java
@@ -1,7 +1,6 @@
 package com.prax19.budgetguardapi;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,7 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @Configuration
-@EnableAutoConfiguration
 @EnableJpaRepositories(basePackages = "com.prax19.repositories")
 @ComponentScan(basePackages = {"com.prax19"})
 @EntityScan(basePackages = {"com.prax19.entities"})
