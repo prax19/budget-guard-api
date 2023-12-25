ac1b8544c38fa0bd8a1d3c39e7c3f3472fde6bf1
prax19 <fhcarpaty2@gmail.com>
2023-12-26 00:31:50 +0100

    Add `H2Configuration` for remote database access

---
+++ src/main/java/com/prax19/budgetguardapi/H2Configuration.java
@@ -0,0 +1,17 @@
+package com.prax19.budgetguardapi;
+
+import org.h2.tools.Server;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.sql.SQLException;
+
+@Configuration
+public class H2Configuration {
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp");
+    }
+
+}
