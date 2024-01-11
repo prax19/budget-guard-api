063bdd238a28341efcf122ff7c797406f83d018d
prax19 <fhcarpaty2@gmail.com>
2024-01-11 21:02:07 +0100

    Update user exception handling

--- src/main/java/com/prax19/services/RegistrationService.java
+++ src/main/java/com/prax19/services/RegistrationService.java
@@ -1,5 +1,7 @@
 package com.prax19.services;
 
+import com.prax19.services.validators.EmailValidator;
+import com.prax19.services.validators.PasswordValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,21 +12,24 @@ import com.prax19.requests.RegistrationRequest;
 @Service
 public class RegistrationService {
 
-    private final static String EMAIL_NOT_VALID_MSG = "email '%s' not valid";
+    private final static String EMAIL_NOT_VALID_MSG = "login '%s' not valid";
+    private final static String PASSWORD_NOT_VALID_MSG = "password not valid";
 
     @Autowired
     private EmailValidator emailValidator;
+
+    @Autowired
+    private PasswordValidator passwordValidator;
 
     @Autowired
     private UserDetailsService userDetailsService;
 
     public UserDetails register(RegistrationRequest request) {
 
-        boolean isValidEmail = emailValidator.test(request.getEmail());
-
-        if(!isValidEmail) {
+        if(!emailValidator.test(request.getEmail()))
             throw new IllegalStateException(String.format(EMAIL_NOT_VALID_MSG, request.getEmail()));
-        }
+        if(!passwordValidator.test(request.getPassword()))
+            throw new IllegalStateException(String.format(PASSWORD_NOT_VALID_MSG));
 
         return userDetailsService.signUpUser(
             new UserDetails(
--- src/main/java/com/prax19/services/UserDetailsService.java
+++ src/main/java/com/prax19/services/UserDetailsService.java
@@ -1,6 +1,8 @@
 package com.prax19.services;
 
 import com.prax19.entities.UserDetails;
+import com.prax19.exceptions.user.UserAlreadyExistsException;
+import com.prax19.exceptions.user.UserNotFoundException;
 import com.prax19.repositories.UserDetailsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -26,22 +28,21 @@ public class UserDetailsService implements org.springframework.security.core.use
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         return userDetailsRepository.findByEmail(email)
             .orElseThrow(() -> 
-                new UsernameNotFoundException(String.format(USER_NOT_FOUND_EMAIL_MSG, email))
+                new UserNotFoundException(String.format(USER_NOT_FOUND_EMAIL_MSG, email))
             );
     }
 
     public UserDetails loadUserById(Long id) throws NoSuchElementException {
         return userDetailsRepository.findById(id)
-                .orElseThrow(() ->
-                        new NoSuchElementException(String.format(USER_NOT_FOUND_ID_MSG, id))
-                );
+            .orElseThrow(() ->
+                    new UserNotFoundException(String.format(USER_NOT_FOUND_ID_MSG, id))
+            );
     }
 
     public UserDetails signUpUser(UserDetails userDetails) {
         boolean userExists = userDetailsRepository.findByEmail(userDetails.getEmail()).isPresent();
-
         if(userExists)
-            throw new IllegalStateException(String.format(USER_ALREADY_EXISTS_MSG, userDetails.getEmail()));
+            throw new UserAlreadyExistsException(String.format(USER_ALREADY_EXISTS_MSG, userDetails.getEmail()));
 
         String encodedPassword = bCryptPasswordEncoder.encode(userDetails.getPassword());
         userDetails.setPassword(encodedPassword);
